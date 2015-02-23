4bbf058bf52d9c8bab2a8cf1e60973620ca82a33
Yochay Kiriaty <yochayk@microsoft.com>
2015-02-23 06:28:54 +0000

    Update demo from Tokyo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Awesome Web App in node js', text1:'TechEd New Zeland' });
+  res.render('index', { title: 'Awesome Web App in node js', text1:'Azure Websites Roadshow' });
 });
 
 module.exports = router;
